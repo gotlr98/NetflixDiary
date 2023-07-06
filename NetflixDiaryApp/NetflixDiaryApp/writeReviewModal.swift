f3cd1b57ab8e2ec54d9720a74a981598df9ccb21
gotlr98 <gotlr98@naver.com>
2023-07-06 14:23:38 +0900

    feat : add search textfield & search event

--- NetflixDiaryApp/NetflixDiaryApp/writeReviewModal.swift
+++ NetflixDiaryApp/NetflixDiaryApp/writeReviewModal.swift
@@ -12,6 +12,8 @@ import SwiftUI
 
 class writeReviewModal: UIViewController{
     
+    var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         if let sheetPresentationController = sheetPresentationController{
             sheetPresentationController.detents = [.medium(), .large()]
@@ -30,8 +32,8 @@ class writeReviewModal: UIViewController{
         
         self.view.addSubview(cancelBtn)
         cancelBtn.translatesAutoresizingMaskIntoConstraints = false
-        cancelBtn.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        cancelBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cancelBtn.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        cancelBtn.heightAnchor.constraint(equalToConstant: 40).isActive = true
         cancelBtn.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
         cancelBtn.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
         
@@ -42,14 +44,58 @@ class writeReviewModal: UIViewController{
         
         self.view.addSubview(registerBtn)
         registerBtn.translatesAutoresizingMaskIntoConstraints = false
-        registerBtn.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        registerBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        registerBtn.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        registerBtn.heightAnchor.constraint(equalToConstant: 40).isActive = true
         registerBtn.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-        registerBtn.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: 20).isActive = true
+        registerBtn.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: -30).isActive = true
+        
+        
+        searchTextField = .init(frame: .init())
+        
+        searchTextField.placeholder = "검색할 작품을 입력해주세요"
+        searchTextField.font = UIFont.systemFont(ofSize: 20)
+        searchTextField.borderStyle = .line
+        searchTextField.autocorrectionType = .no
+        searchTextField.keyboardType = .default
+        searchTextField.returnKeyType = .done
+        searchTextField.autocapitalizationType = .none
+//        searchTextField.delegate =
+        
+        self.view.addSubview(searchTextField)
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchTextField.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        searchTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        searchTextField.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        searchTextField.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+        
+        let searchButton: UIButton = .init(frame: .init())
+        
+        searchButton.backgroundColor = .orange
+        
+        searchButton.setTitle("닫기", for: .normal)
+        searchButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        
+        self.view.addSubview(searchButton)
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        searchButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        searchButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        searchButton.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
+        searchButton.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        
+        searchButton.addTarget(self, action: #selector(search), for: .touchUpInside)
     }
     
     @objc func cancel(){
         self.dismiss(animated: true)
+    }
+    
+    @objc func search(){
+        guard let text = searchTextField.text else{
+            return
+        }
+        print(text)
     }
 }
 //
