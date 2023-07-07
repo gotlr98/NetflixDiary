82b7c90bec12d220131cb70c0f1bca41b01427ef
gotlr98 <gotlr98@naver.com>
2023-07-07 15:44:16 +0900

    feat : push data -> searchMovie

--- NetflixDiaryApp/NetflixDiaryApp/searchMovie.swift
+++ NetflixDiaryApp/NetflixDiaryApp/searchMovie.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class searchMovie: UIViewController{
     
-    override func viewDidLoad() {
-        
+    var movie_title: String
+    
+    init(movie_title: String){
+        self.movie_title = movie_title
+        super.init(nibName: nil, bundle: nil)
     }
     
-    func recieveData(response: String) {
-        
+    required init?(coder: NSCoder) {
+        fatalError("error")
     }
+    override func viewDidLoad() {
+        
+        print(movie_title)
+    }
+    
+
 }
--- NetflixDiaryApp/NetflixDiaryApp/writeReviewModal.swift
+++ NetflixDiaryApp/NetflixDiaryApp/writeReviewModal.swift
@@ -19,26 +19,15 @@ class writeReviewModal: UIViewController{
     var searchTextField: UITextField!
     var delegate: SendDataDelegate?
     var img_url: [String] = []
+    
     var searchButton: UIButton = .init(frame: .init())
     
     
     override func viewDidLoad() {
         self.view.backgroundColor = .gray
         
-        let cancelBtn: UIButton = .init(frame: .init())
         let registerBtn: UIButton = .init(frame: .init())
         
-        cancelBtn.backgroundColor = .orange
-        
-        cancelBtn.setTitle("닫기", for: .normal)
-        cancelBtn.addTarget(self, action: #selector(cancel), for: .touchUpInside)
-        
-        self.view.addSubview(cancelBtn)
-        cancelBtn.translatesAutoresizingMaskIntoConstraints = false
-        cancelBtn.widthAnchor.constraint(equalToConstant: 120).isActive = true
-        cancelBtn.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        cancelBtn.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-        cancelBtn.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
         
         registerBtn.backgroundColor = .orange
         
@@ -50,7 +39,7 @@ class writeReviewModal: UIViewController{
         registerBtn.widthAnchor.constraint(equalToConstant: 120).isActive = true
         registerBtn.heightAnchor.constraint(equalToConstant: 40).isActive = true
         registerBtn.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-        registerBtn.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: -30).isActive = true
+        registerBtn.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
         
         
         searchTextField = .init(frame: .init())
@@ -78,13 +67,13 @@ class writeReviewModal: UIViewController{
         
         searchButton.backgroundColor = .orange
         
-        searchButton.setTitle("닫기", for: .normal)
+//        searchButton.setTitle("닫기", for: .normal)
         searchButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
         
         self.view.addSubview(searchButton)
         searchButton.translatesAutoresizingMaskIntoConstraints = false
         searchButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        searchButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        searchButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         searchButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         searchButton.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
         searchButton.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
@@ -121,9 +110,11 @@ class writeReviewModal: UIViewController{
             return
         }
         else{
-            requestNet(name: searchTextField.text!)
             
-            self.navigationController?.pushViewController(searchMovie(), animated: false)
+//            requestNet(name: searchTextField.text!)
+            
+            let vc = searchMovie(movie_title: searchTextField.text!)
+            self.navigationController?.pushViewController(vc, animated: false)
         }
         
     }
