35e8964b16851edfd327891630883bddfaa2166b
gotlr98 <gotlr98@naver.com>
2023-07-11 19:02:00 +0900

    feat : make slider

--- NetflixDiaryApp/NetflixDiaryApp/writeReviewModal.swift
+++ NetflixDiaryApp/NetflixDiaryApp/writeReviewModal.swift
@@ -22,8 +22,13 @@ class writeReviewModal: UIViewController{
     var delegate: SendDataDelegate?
     var img_url: [String] = []
     var select_title: String = ""
+    var isButtonClicked: Bool = false
     
     var searchButton: UIButton = .init(frame: .init())
+    
+    lazy var imageScrollView = UIScrollView()
+    let imagePageControl = UIPageControl()
+    let imageNumberLabel = UILabel()
     
     
     override func viewDidLoad() {
@@ -64,14 +69,14 @@ class writeReviewModal: UIViewController{
         searchTextField.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         searchTextField.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
         
-//        let searchButton: UIButton = .init(frame: .init())
+        let searchButton: UIButton = .init(frame: .init())
         
 //        let searchButton: UIButton = .init(frame: .init())
         
         searchButton.backgroundColor = .orange
         
-//        searchButton.setTitle("닫기", for: .normal)
         searchButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        searchButton.addTarget(self, action: #selector(self.textFieldDidChanacge(_:)), for: .editingChanged)
         
         self.view.addSubview(searchButton)
         searchButton.translatesAutoresizingMaskIntoConstraints = false
@@ -81,12 +86,75 @@ class writeReviewModal: UIViewController{
         searchButton.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
         searchButton.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
         
-        let slide = ImageSlideshow()
-        slide.setImageInputs([KingfisherSource(urlString: "https://image.tmdb.org/t/p/original/9WF6TxCYwdiZw51NM92ConaQz1w.jpg"),
-                              KingfisherSource(urlString: "https://image.tmdb.org/t/p/original/9WF6TxCYwdiZw51NM92ConaQz1w.jpg")])
+        
+        self.view.addSubview(imageScrollView)
+        self.view.addSubview(imagePageControl)
+        self.view.addSubview(imageNumberLabel)
+        
+        imageScrollView.translatesAutoresizingMaskIntoConstraints = false
+        imageScrollView.isPagingEnabled = true
+        imageScrollView.showsHorizontalScrollIndicator = false
+        imageScrollView.delegate = self
 
+        
+        imagePageControl.translatesAutoresizingMaskIntoConstraints = false
+        imagePageControl.currentPage = 0
+        imagePageControl.pageIndicatorTintColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.5)
+        imagePageControl.currentPageIndicatorTintColor = .white
+        imagePageControl.hidesForSinglePage = true
+
+        
+        imageNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        imageNumberLabel.backgroundColor = UIColor(red: 34/255, green: 34/255, blue: 34/255, alpha: 0.5)
+        imageNumberLabel.layer.cornerRadius = 14
+        imageNumberLabel.clipsToBounds = true
+        imageNumberLabel.text = "0/1"
+        imageNumberLabel.font = .systemFont(ofSize: 12, weight: .semibold)
+        imageNumberLabel.textColor = .white
+        
+
+        img_url.append("https://image.tmdb.org/t/p/w220_and_h330_face/9WF6TxCYwdiZw51NM92ConaQz1w.jpg")
+        img_url.append("https://image.tmdb.org/t/p/w220_and_h330_face/9WF6TxCYwdiZw51NM92ConaQz1w.jpg")
+        img_url.append("https://image.tmdb.org/t/p/w220_and_h330_face/9WF6TxCYwdiZw51NM92ConaQz1w.jpg")
+        img_url.append("https://image.tmdb.org/t/p/w220_and_h330_face/9WF6TxCYwdiZw51NM92ConaQz1w.jpg")
+        
+        setImageSlider(images: img_url)
+        
+        
+//        let slide = ImageSlideshow()
+//
+//        lazy var labelView: UIView = {
+//            let view = UIView()
+//            view.backgroundColor = UIColor(white: 0.3, alpha: 0.3)
+//            return view
+//        }()
+//
+//        slide.contentScaleMode = .scaleAspectFit
+//        slide.pageIndicatorPosition = .init(horizontal: .right(padding: 20), vertical: .customBottom(padding: 20))
+//
+//        let labelIndicator = LabelPageIndicator()
+//        labelIndicator.textColor = .white
+//        labelIndicator.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+//
+//        slide.addSubview(labelView)
+//        self.view.addSubview(slide)
+//
+//        slide.translatesAutoresizingMaskIntoConstraints = false
+//        slide.widthAnchor.constraint(equalToConstant: 300).isActive = true
+//        slide.heightAnchor.constraint(equalToConstant: 200).isActive = true
+//        slide.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 200).isActive = true
+//        slide.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+//
+//        slide.activityIndicator = DefaultActivityIndicator(style: .medium, color: .white)
+        
+//        if isButtonClicked{
+//            DispatchQueue.main.async{
+//                slide.setImageInputs([KingfisherSource(urlString: "https://image.tmdb.org/t/p/w220_and_h330_face/9WF6TxCYwdiZw51NM92ConaQz1w.jpg")!])
+//            }
+//        }
+        
     }
-    
+        
     override func viewDidAppear(_ animated: Bool) {
         
         super.viewDidAppear(animated)
@@ -106,6 +174,10 @@ class writeReviewModal: UIViewController{
         self.dismiss(animated: true)
     }
     
+    @objc func textFieldDidChanacge(_ sender: Any?) {
+        self.img_url = []
+    }
+    
     @objc func search(){
         
         let alert = UIAlertController(title: "Error", message: "검색어를 입력해 주세요", preferredStyle: UIAlertController.Style.alert)
@@ -120,47 +192,119 @@ class writeReviewModal: UIViewController{
         }
         else{
             
-//            requestNet(name: searchTextField.text!)
+//            Task{
+//                await self.requestNet(name: self.searchTextField.text!)
+//
+//            }
+//            print(self.img_url)
             
-            let vc = searchMovie(movie_title: searchTextField.text!)
             
-            self.navigationController?.pushViewController(vc, animated: false)
+//            let vc = searchMovie(movie_title: searchTextField.text!)
+//            self.navigationController?.pushViewController(vc, animated: false)
         }
+        self.isButtonClicked = true
         
     }
     
+    func requestNet(name: String) async {
+        let headers = [
+          "accept": "application/json",
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGNiMmEwNTRjYTZmMTEyZDY2YjFlODE2ZTIzOWVlNiIsInN1YiI6IjY0OWJkOWUwNzdjMDFmMDBjYTVhNzkwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YXEMCu81V6_te2LwVDZiNCFrn1GUZeCcTAj4WYTGwug"
+        ]
+
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=1&sort_by=popularity.desc&with_watch_providers=providers%3A8")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
         
-    func getTopMostViewController() -> UIViewController? {
-        var topMostViewController = UIApplication.shared.windows[0].rootViewController
+        let API_KEY = "e8cb2a054ca6f112d66b1e816e239ee6"
+        var movieSearchURL = URLComponents(string: "https://api.themoviedb.org/3/search/movie?")
 
-        while let presentedViewController = topMostViewController?.presentedViewController {
-            topMostViewController = presentedViewController
-        }
+        // 쿼리 아이템 정의
+        let apiQuery = URLQueryItem(name: "api_key", value: API_KEY)
+        let languageQuery = URLQueryItem(name: "language", value: "ko-KR")
+        let searchQuery = URLQueryItem(name: "query", value: name)
+        
+        movieSearchURL?.queryItems?.append(apiQuery)
+        movieSearchURL?.queryItems?.append(languageQuery)
+        movieSearchURL?.queryItems?.append(searchQuery)
+        
+        guard let requestMovieSearchURL = movieSearchURL?.url else { return }
+        
+        let config = URLSessionConfiguration.default
 
-        return topMostViewController
+        // session 설정
+        let session = URLSession(configuration: config)
+        
+            let dataTask = session.dataTask(with: requestMovieSearchURL, completionHandler: { (data, response, error) -> Void in
+              if (error != nil) {
+                print(error as Any)
+              } else {
+                  guard let resultData = data else { return }
+                  do{
+                      let decoder = JSONDecoder()
+                      let respons = try decoder.decode(Response.self, from: resultData)
+                      let searchMovie = respons.result
+                      
+                      for i in searchMovie{
+    //                      print("영화 제목 : \(i.title ?? "")")
+    //                      print("영화 평점 : \(i.rating ?? 0)")
+    //                      print("영화 줄거리 : \(i.summary ?? "")")
+    //                      print("포스터 경로 : \(i.post ?? "")")
+    //
+    //                      print("--------------------------")
+                          
+                          self.img_url.append("https://image.tmdb.org/t/p/w220_and_h330_face" + i.post!)
+                      }
+                      
+                  }catch let error{
+                      print(error.localizedDescription)
+                  }
+              }
+            })
+            dataTask.resume()
+        
     }
+    func setImageSlider(images: [String]) { // scrolliVew에 imageView 추가하는 함수
+        for index in 0..<images.count {
+            
+            let url = URL(string: images[index])
+            var image: UIImage?
+            
+            DispatchQueue.global().async{
+                let data = try? Data(contentsOf: url!)
+                    DispatchQueue.main.async {
+                        image = UIImage(data: data!)
+                    }
+            }
+            
+            let imageView = UIImageView()
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFit
+            imageView.layer.cornerRadius = 5
+            imageView.clipsToBounds = true
+
+            let xPosition = self.view.frame.width * CGFloat(index)
+
+            imageView.frame = CGRect(x: xPosition,
+                                   y: 0,
+                                   width: self.view.frame.width,
+                                   height: self.view.frame.width)
+
+            imageScrollView.contentSize.width = self.view.frame.width * CGFloat(index+1)
+            imageScrollView.addSubview(imageView)
+        }
+      }
+
+}
+
+extension writeReviewModal: UIScrollViewDelegate {
+  func scrollViewDidScroll(_ scrollView: UIScrollView) { // scrollView가 스와이프 될 때 발생 될 이벤트
+    self.imagePageControl.currentPage = Int(round(imageScrollView.contentOffset.x / UIScreen.main.bounds.width))
+    self.imageNumberLabel.text = "\(imagePageControl.currentPage)/\(imagePageControl.numberOfPages)"
+  }
 }
 
 
-//
-//#if DEBUG
-//import SwiftUI
-//struct ViewControllerRepresentable: UIViewControllerRepresentable {
-//
-//    // update
-//    func updateUIViewController(_ uiViewController: UIViewController, context: Context){
-//
-//    }
-//    // makeui
-//    @available(iOS 13.0, *)
-//    func makeUIViewController(context: Context) -> UIViewController {
-//        ViewController()
-//    }
-//}
-//
-//struct ViewController_Previews: PreviewProvider {
-//    static var previews: some View{
-//        ViewControllerRepresentable().previewDisplayName("아이폰 11")
-//    }
-//}
-//#endif
+
